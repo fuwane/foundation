be52cde8b53b6a9e8fbf88712ba1d266fb54de80
Takagi Tasuku <tasuren@outlook.jp>
2023-04-30 21:37:31 +0900

    update: いくつかをsys等から移植。

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+//! FuwaNe Foundation - Event
+
+extern crate serde;
+use serde::{ Serialize, Deserialize };
+use serde_big_array::BigArray;
+
+
+#[cfg(target_arch="wasm32")]
+pub mod constants {
+    pub const SAMPLE_RATE: usize = 48000;
+    pub const AUDIO_FRAME_RATE: usize = 50;
+    pub const MONO_FRAME_SIZE: usize = SAMPLE_RATE / AUDIO_FRAME_RATE;
+    pub const STEREO_FRAME_SIZE: usize = 2 * MONO_FRAME_SIZE;
+    pub const STEREO_FRAME_BYTE_SIZE: usize = STEREO_FRAME_SIZE * std::mem::size_of::<f32>();
+}
+
+#[cfg(not(target_arch="wasm32"))]
+pub mod constants {
+    pub use songbird::constants::{
+        SAMPLE_RATE_RAW as SAMPLE_RATE, AUDIO_FRAME_RATE,
+        MONO_FRAME_SIZE, STEREO_FRAME_SIZE, STEREO_FRAME_BYTE_SIZE
+    };
+}
+
+
+pub mod commnication {
+    use std::sync::Arc;
+    
+    use tokio::sync::{ mpsc::{ Sender, Receiver, channel }, Mutex as AioMutex };
+    use once_cell::sync::Lazy;
+
+    
+    pub struct Channel<T> {
+        pub tx: Sender<T>,
+        pub rx: Arc<AioMutex<Receiver<T>>>
+    }
+
+    pub fn create_lazy_channel<T>() -> Lazy<Channel<T>> {
+        Lazy::new(|| {
+            let (tx, rx) = channel(128); Channel {
+                tx: tx, rx: Arc::new(AioMutex::new(rx))
+            }
+        })
+    }
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Context {
+    #[serde(with="BigArray")]
+    pub buffer: [u8;constants::STEREO_FRAME_BYTE_SIZE]
+}
